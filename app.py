ec04afbd1cd6a8ac40bdfd5e96e6b0e8ef362d9f
Matt Parkinson <matt.parkinson@redhat.com>
2022-05-15 10:34:22 +1000

    Add health endpoint

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+
+@app.route('/healthz')
+def healthz():
+    return "Health: OK"
+
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
